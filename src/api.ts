6ce00bbcbcacfe7689c8569a42eac38ee6dc4a5e
Charles-Alexandre Bedard <bedard-charles@hotmail.com>
2021-10-31 16:21:01 -0400

    test pre-commit hook

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,12 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+const unuser_var = 20
+
 app.listen(port, () => {
-  console.log(`App is running, listening on port: ${port}`);
+    console.log(`App is running, listening on port: ${port}`);
+});
+
+app.get('/users', function (req, res) {
+    res.send('requested users');
 });
