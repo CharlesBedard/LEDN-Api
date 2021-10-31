cce036672cc0486791979ddf575491544ddb9ac5
Charles-Alexandre Bedard <bedard-charles@hotmail.com>
2021-10-31 16:55:06 -0400

    dockerize api and mongodb

--- src/api.ts
+++ src/api.ts
@@ -3,8 +3,6 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-const unuser_var = 20;
-
 app.listen(port, () => {
     console.log(`App is running, listening on port: ${port}`);
 });
